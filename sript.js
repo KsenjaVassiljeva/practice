ce669ef464ed6a6906246a94ab7374ca51624571
Ritana . <ksenja.vassiljeva@ivkhk.ee>
2026-02-24 04:24:47 +0200

    No light and dark mode

--- sript.js
+++ sript.js
@@ -1,19 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll("[data-animate]");
-
-    function animateCards() {
-        const triggerBottom = window.innerHeight * 0.85;
-
-        cards.forEach(card => {
-            const cardTop = card.getBoundingClientRect().top;
-
-            if (cardTop < triggerBottom) {
-                card.style.opacity = "1";
-                card.style.transform = "translateY(0)";
-            }
-        });
-    }
-
-    window.addEventListener("scroll", animateCards);
-    animateCards(); // для случаев, если уже прокручено
+const projects = document.querySelectorAll('.project-item');
+projects.forEach(project => {
+    project.addEventListener('click', () => {
+        alert(`Вы выбрали проект: ${project.textContent}`);
+    });
 });
